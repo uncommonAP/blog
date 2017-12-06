5bd3e4d64212f7270f8716459cb5e9ed4f28c249
uncommonAP <aaron.paul_r@outlook.com>
2017-12-06 15:32:42 -0500

    config webpacker to support react

---
+++ app/javascript/react/BlogSite.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class BlogSite extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>Hello from React Component!</div>
+    )
+  }
+}
+
+export default BlogSite
