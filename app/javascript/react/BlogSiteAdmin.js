a350b48f684074a4eb6dbef662121889acccd6b0
uncommonAP <aaron.paul_r@outlook.com>
2017-12-08 13:23:22 -0500

    prepare admin environment

--- app/javascript/react/BlogSiteAdmin.js
+++ app/javascript/react/BlogSiteAdmin.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import { Switch, Route, NavLink } from 'react-router-dom'
+import ArticlesIndexAdmin from './articlesAdmin/containers/ArticlesIndexAdmin'
+import ArticleShowAdmin from './articlesAdmin/containers/ArticlesIndexAdmin'
+import NewArticle from './articlesAdmin/containers/NewArticle'
 
 class BlogSiteAdmin extends Component {
   constructor(props) {
@@ -7,7 +11,15 @@ class BlogSiteAdmin extends Component {
 
   render() {
     return(
-      <div>This is the admin page!</div>
+      <div>
+        <Switch>
+          <Route strict path='admin/articles/' component={ArticlesIndexAdmin} />
+          <Route strict path='admin/show/:id/' component={ArticleShowAdmin} />
+          <Route strict path='admin/create' component={NewArticle} />
+          Welcome to your blog site! What would you like to do?
+        </Switch>
+
+      </div>
     )
   }
 }
