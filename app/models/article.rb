8567bc6b1088f4fb5412a0b96f2cd90c9b538ce9
uncommonAP <aaron.paul_r@outlook.com>
2017-12-15 15:14:59 -0500

    create publish method on article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  def publish(user)
+    if user.admin?
+      self.update(draft: false, published: true)
+    end
+  end
 end
