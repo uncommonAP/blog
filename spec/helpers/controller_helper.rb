96c4fea0d121bf36c6b798eb2ef04b88347469aa
uncommonAP <aaron.paul_r@outlook.com>
2017-12-15 11:28:20 -0500

    add stubbed login for public and admin users

--- spec/helpers/controller_helper.rb
+++ spec/helpers/controller_helper.rb
@@ -16,4 +16,15 @@ module ControllerHelper
     post :create, params: params
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def log_in_as_user_admin(user)
+    admin_user = double(:user, :admin? => true)
+    controller.stub(:current_user) { admin_user }
+  end
+
+  def log_in_as_user_public(user)
+    public_user = double(:user, :admin? => false)
+    controller.stub(:current_user) { public_user }
+  end
+
 end
