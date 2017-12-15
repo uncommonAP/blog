3a73b8e4e36b69cee657f70c3119add35c9f54ec
uncommonAP <aaron.paul_r@outlook.com>
2017-12-15 15:18:02 -0500

    controller methods `create`, `update`, and `publish`

---
+++ app/controllers/api/v1/admin/articles_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::Admin::ArticlesController < AdminFilesController
+  before_action :verify_admin
+
+  def create
+    draft = Article.new(article_params)
+    if draft.save
+      render json: draft
+    else
+      alert messages: draft.errors.full_messages
+    end
+  end
+
+  def update
+    article = Article.find(params[:id])
+    if article.update(article_params)
+      render json: article
+    end
+  end
+
+  def publish
+    article = Article.find(params[:id])
+    binding.pry
+    if article.publish(current_user)
+      render json: article
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
